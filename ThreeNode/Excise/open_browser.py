e1936f6d44b32e5153a8740d1329ab22813e1ae5
16348104 <16348104@qq.com>
2019-12-04 17:56:20 +0800

    max

--- ThreeNode/Excise/open_browser.py
+++ ThreeNode/Excise/open_browser.py
@@ -23,14 +23,30 @@ def open(browse):
 # 获得URL
 def get_url(url):
     driver = open('chrome')
+    # driver.maximize_window()
     if driver != None:
         if 'https://' in url:
             driver.get(url)
-            time.sleep(10)
+            time.sleep(5)
         else:
             print('url有错误')
     else:
         print('case失败')
 
         # driver.quit()
-get_url('https://www.imooc.com/')
+
+
+def handle_windows(*args):
+    driver = open('chrome')
+    # get_url('https://www.imooc.com/')
+    val = len(args)
+    if val == 1:
+        if args[0] == 'max':
+            driver.maximize_window()
+        else:
+            driver.minimize_window()
+    elif val == 2:
+        driver.set_window_size(args[0], args[1])
+    else:
+        print('浏览器传参错误')
+handle_windows('max')
